6390d7b6a8f578fa4c1e590dc4330ae71f79d17f
Borthvader <104023232+borthvader@users.noreply.github.com>
2022-05-24 19:33:46 -0400

    Finished Game and added timer

--- assets/script.js
+++ assets/script.js
@@ -64,28 +64,48 @@ var scoreBox = document.querySelector("#score");
 var leaderboardBox = document.querySelector("#leaderboard");
 var result = document.querySelector("#result");
 var resultMsg = document.querySelector("#result-msg");
+var timeClock = document.querySelector("#time");
+var score = document.querySelector("#final-score");
+var confirmBtn = document.querySelector("#confirm-button");
+var input = document.querySelector("#initials");
 // creates global variables for timer and question number
-var intervalID;
+var intervalId;
 var time;
 var currentQuestion;
 
+// hides unused elements when not in use
 function hideBoxes() {
-   quizStart.setAttribute("hidden", true);
-   scoreBox.setAttribute("hidden",true)
-   leaderboardBox.setAttribute("hidden",true)
+   quizStart.style.display = "none";
+   questionBox.style.display = "none";
+   scoreBox.style.display = "none";
+   leaderboardBox.style.display = "none";
 }
 
 
 quizStart.addEventListener("click", function(){
     hideBoxes();
-    questionBox.removeAttribute("hidden");
+    questionBox.style.display = "block";
     // ensures that the first element of the array is called at the start of the quiz
     currentQuestion = 0;
     // calls the function to display questions
     showQuestion();
     time = questions.length * 15;
-
+    intervalId = setInterval(timer, 1000);
+    showTime(); 
 });
+// reduces time by one at a set interval, otherwise run finish function
+function timer(){
+    time--;
+    showTime();
+    if (time < 1){
+        finish();
+    }
+}
+// display timer on page
+function showTime(){
+    timeClock.textContent = time;
+}
+// reveal the question and answer options
 function showQuestion(){
     var question = questions[currentQuestion];
     var choices = question.choices;
@@ -102,6 +122,7 @@ function showQuestion(){
 
 document.querySelector("#quiz-buttons").addEventListener('click', checkAnswer);
 
+// compares the text of the selected button with the answer provided
 function correctChoice(choiceButton){
     return choiceButton.textContent === questions[currentQuestion].answer;
 }
@@ -113,12 +134,49 @@ function checkAnswer(eventObject){
         resultMsg.textContent = "You're Right!"
      } else {
          resultMsg.textContent = "Not this time!"
+        if(time >= 10){
+            time = time -10;
+            showTime();
+        } else{
+            time = 0;
+            showTime();
+            finish();
+        }
     }
     currentQuestion++;
     if (currentQuestion < questions.length) {
         showQuestion();
+    } else{
+        finish();
     }
 }
+function finish(){
+    clearInterval(intervalId);
+    hideBoxes();
+    scoreBox.style.display = "block";
+    score.textContent = time;
+}
+// confirmBtn.addEventListener("click", saveScore)
+
+// function saveScore(event){
+//     event.preventDefault();
+//     var highscoreEl = {
+//         initials: input.value,
+//         score: time,
+//     };
+//         hideBoxes();
+//         leaderboardBox.style.display = "block";
+//         localStorage.setItem("highscoreEl", JSON.stringify(highscoreEl));
+//     renderHighscore();
+    
+// }
+
+// function renderHighscore(){
+//     var lastscore = JSON.parse(localStorage.getitem("#score-list"));
+//     if (lastscore !==null){
+//         document.querySelector("#score-list").textContent = lastscore.initials + lastscore.score
+//     } 
+// }
 
 
 
